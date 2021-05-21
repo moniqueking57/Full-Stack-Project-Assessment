b0e2c6ab5cc8a0e0b6aa133b66ca131ca57dbd55
Monique <moniqueking57@gmail.com>
2021-05-21 20:26:58 +0100

    started 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,62 @@
-import "./App.css";
+import {useState} from 'react';
+import './App.css';
+import example from './exampleresponse.json';
+import Video from './video.js';
 
 function App() {
+  console.log(example);
+  const [videos, setVideos] = useState(example);
+  function handleLike(e, id) {
+    const index = videos.findIndex((item) => item.id === id);
+    const patchedVideo = {...videos[index]};
+    console.log(patchedVideo);
+    const updatedVideo = {
+      id: patchedVideo.id,
+      title: patchedVideo.title,
+      rating: patchedVideo.rating + 1,
+      url: patchedVideo.url,
+    };
+    const newVideos = [
+      ...videos.slice(0, index),
+      updatedVideo, // comment to delete something similar but no slice
+      ...videos.slice(index + 1),
+    ];
+    console.log(newVideos);
+    setVideos(newVideos);
+  }
+  function handleDislike(e, id) {
+    const index = videos.findIndex((item) => item.id === id);
+    const patchedVideo = {...videos[index]};
+    console.log(patchedVideo);
+    const updatedVideo = {
+      id: patchedVideo.id,
+      title: patchedVideo.title,
+      rating: patchedVideo.rating - 1,
+      url: patchedVideo.url,
+    };
+    const newVideos = [
+      ...videos.slice(0, index),
+      updatedVideo, // comment to delete something similar but no slice
+      ...videos.slice(index + 1),
+    ];
+    console.log(newVideos);
+    setVideos(newVideos);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      {videos.map((item) => (
+        <Video
+          title={item.title}
+          id={item.id}
+          url={item.url}
+          rating={item.rating}
+          handleLike={handleLike}
+          handleDislike={handleDislike}
+        />
+      ))}
     </div>
   );
 }
